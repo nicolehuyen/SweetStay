47884cd30874f60ae051e0ac6c583fcd9209c720
Nicole Le <nicole.huyen.le@gmail.com>
2023-11-26 18:32:12 -0600

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
@@ -39,5 +41,13 @@ router.use(restoreUser);
 //     return res.json(req.user);
 //   }
 // );
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
