ddfdee6be7e562d04aca3fbeb47ff850613ba89b
Nicole Le <nicole.huyen.le@gmail.com>
2023-11-27 17:57:22 -0600

    edit route

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,15 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
+const routes = require('./routes');
+
 const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
 const app = express();
-const routes = require('./routes');
 
 app.use(morgan('dev'));
 
