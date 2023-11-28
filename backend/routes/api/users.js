ce4749611adb9c9b11473ae43b64a5c44f2f3e27
Nicole Le <nicole.huyen.le@gmail.com>
2023-11-27 19:11:43 -0600

    update

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,30 +10,6 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-// Sign up
-router.post(
-    '/',
-    async (req, res) => {
-      const { firstName, lastName, email, password, username } = req.body;
-      const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
-
-      const safeUser = {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        username: user.username,
-      };
-
-      await setTokenCookie(res, safeUser);
-
-      return res.json({
-        user: safeUser
-      });
-    }
-);
-
 const validateSignup = [
     check('email')
       .exists({ checkFalsy: true })
