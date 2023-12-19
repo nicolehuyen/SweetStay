93405f7ca87f2d89f5b5e7856cff94efd1bcaa77
Nicole Le <nicole.huyen.le@gmail.com>
2023-12-19 01:43:02 -0600

    store/component spots

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation({ isLoaded }) {
   return (
     <ul>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/">Home</NavLink>
       </li>
       {isLoaded && (
         <li>
