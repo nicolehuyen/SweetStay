a994863a7e424233ae36740f560ab440d05a56d3
Nicole Le <nicole.huyen.le@gmail.com>
2023-12-20 01:00:36 -0600

    start spot details

--- frontend/src/components/Spots/SpotDetails.jsx
+++ frontend/src/components/Spots/SpotDetails.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getSpotByIdThunk } from "../../store/spots";
+
 function SpotDetails() {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getSpotByIdThunk())
+    }, [dispatch])
+
     return (
         <h1>Spot Details</h1>
     )
